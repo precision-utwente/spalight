699b1c9e3469fca8b7fc3bb3d1b977734731dbb8
kramsevan <m.naves@utwente.nl>
2018-05-15 12:07:33 +0200

    fix complt script
    fix complt script

--- spacarlight.m
+++ spacarlight.m
@@ -617,7 +617,7 @@ end
 if      (id_ini && id_add);      pr_add = sprintf('%s\n\nITERSTEP\t10\t%3u\t0.0000005\t1\t3\t%3u',pr_add,steps,steps);    %if initial and aditional loading/displacement
 elseif  (id_ini && ~id_add);     pr_add = sprintf('%s\n\nITERSTEP\t10\t1\t0.0000005\t1\t1\t%3u',pr_add,steps);    %if initial loading/displacement
 elseif  (~id_ini && id_add);     pr_add = sprintf('%s\n\nITERSTEP\t10\t%3u\t0.0000005\t1\t3\t0',pr_add,steps);     %if initial loading/displacement
-else                             pr_add = sprintf('%s\n\nITERSTEP\t10\t1\t0.0000005\t1\t1\t0',pr_add);  end %#ok<SEPEX> %no loading/displacement
+else                             pr_add = sprintf('%s\n\nITERSTEP\t10\t1\t0.0000005\t1\t1\t0',pr_add);  end  %no loading/displacement
 
 
 %% VISUALIZATION
@@ -1891,13 +1891,13 @@ CMglob=zeros(6,6,tdef);
 CMloc=zeros(6,6,tdef);
 lnp     =getfrsbf([filename '.sbd'],'lnp');
 nx      =getfrsbf([filename '.sbd'],'nx');
-nkdof   =getfrsbf([filename '.sbd'],'nkdof');
 nxp     =getfrsbf([filename '.sbd'],'nxp');
 nep     =getfrsbf([filename '.sbd'],'nep');
 DX_data      =getfrsbf([filename '.sbd'],'dx');
 K0_data      =getfrsbf([filename '.sbm'],'k0');
 G0_data      =getfrsbf([filename '.sbm'],'G0');
 X_data       =getfrsbf([filename '.sbd'],'x');
+
 
 
 % locate the place of the coordinates of the points where the compliance
@@ -1917,6 +1917,9 @@ locv=[lnp(ntr,1:3), lnp(nrot,1:4)];
 % search for the right degrees of freedom
 locdof=[nxp(3)+(1:nxp(4)), nxp(3)+nxp(4)+nep(3)+(1:nep(4))];
 
+if tdef > 1
+    nddof = sqrt(length(K0_data(1,:)));
+end
 
 
 
@@ -1927,14 +1930,20 @@ for tstp=1:tdef
     %DX      =getfrsbf([filename '.sbd'],'dx',tstp);
     %X       =getfrsbf([filename '.sbd'],'x',tstp);
     % reduce DX to the rows needed
-    DX = reshape(DX_data(tstp,:),nx,[]);
-    %K0      =getfrsbf([filename '.sbm'],'k0',tstp);
-    %G0      =getfrsbf([filename '.sbm'],'g0',tstp);
-    K0 = reshape(K0_data(tstp,:),nkdof,[]);
-    G0 = reshape(G0_data(tstp,:),nkdof,[]);
-    
+    if tdef > 1
+        DX = reshape(DX_data(tstp,:),nx,[]);
+        %K0      =getfrsbf([filename '.sbm'],'k0',tstp);
+        %G0      =getfrsbf([filename '.sbm'],'g0',tstp);
+        K0 = reshape(K0_data(tstp,:),nddof,[]);
+        G0 = reshape(G0_data(tstp,:),nddof,[]);
+        X = X_data(tstp,:);
+    else
+        DX = DX_data;
+        K0 = K0_data;
+        G0 = G0_data;
+        X = X_data;
+    end
     DX = DX(locv,locdof);
-    X = X_data(tstp,:);
     
     CMlambda=DX*((K0+G0)\(DX'));
     % Reduce CMlambda to the correct matrices by the lambda matrices
