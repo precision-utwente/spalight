eb5fc4836440547108995552d2b8e051970e0e7f
kramsevan <m.naves@utwente.nl>
2018-09-19 09:34:38 +0200

    included eul2quat
    included eul2quat

--- spacarlight.m
+++ spacarlight.m
@@ -1902,6 +1902,24 @@ warning backtrace on
         
     end
 
+    function q = eul2quat(eul)
+       %conversion from Euler angles (radians) to quaternions
+       %rotation sequence is ZYX (following eul2quat from Robotics System Toolbox)
+       
+       % Pre-allocate output
+       q = zeros(size(eul,1), 4, 'like', eul);
+       
+       % Compute sines and cosines of half angles
+       c = cos(eul/2);
+       s = sin(eul/2);
+       
+       % Construct quaternion
+        q = [c(:,1).*c(:,2).*c(:,3)+s(:,1).*s(:,2).*s(:,3), ...
+            c(:,1).*c(:,2).*s(:,3)-s(:,1).*s(:,2).*c(:,3), ...
+            c(:,1).*s(:,2).*c(:,3)+s(:,1).*c(:,2).*s(:,3), ...
+            s(:,1).*c(:,2).*c(:,3)-c(:,1).*s(:,2).*s(:,3)];
+    end
+
     function [cw, aspect] = cw_values(L,eprops)
         w = eprops.dim(1);
         E = eprops.emod;
